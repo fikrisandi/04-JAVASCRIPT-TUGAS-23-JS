b4a0c244f879d019b75799a9561d471e8f51d990
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-22 20:25:58 +0700

    Tugas 23 JS

---
+++ Tugas 23 JS.js
@@ -0,0 +1,6 @@
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log(angka.filter(function(angka) {
+    return angka > 15;
+    //mencari nilai atau value yang lebih besar dari angka 15
+}));
